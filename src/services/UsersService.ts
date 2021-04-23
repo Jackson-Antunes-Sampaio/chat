229fb29b0c656ccccba078af504a007d133d208e
Jackson Antunes <jackson.antunes.sampaio@gmail.com>
2021-04-23 19:37:46 -0300

    first commit

---
+++ src/services/UsersService.ts
@@ -0,0 +1,45 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository"
+
+
+
+class UsersService {
+
+    private usersRepository: Repository<User>;
+
+    constructor(){
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string){
+
+        //Verificar se o Usuario existe
+        const userExists = await this.usersRepository.findOne({
+            email
+        });
+
+        //se existir retorna o User
+        if(userExists){
+            return userExists;
+        }
+        
+        //Se não existir, salvar no banco de dados
+        const user = this.usersRepository.create({
+            email
+        });
+
+        await this.usersRepository.save(user);
+
+        return user;
+
+        
+    }
+
+    async findByEmail(email: string){
+        const user = await this.usersRepository.findOne({email});
+        return user;
+    }
+}
+
+export {UsersService}
